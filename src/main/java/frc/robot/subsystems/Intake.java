ec13ce85087be73bc35d84da46e15fcd7fdbb926
Keller6738 <155310889+Keller6738@users.noreply.github.com>
2024-07-02 11:13:32 +0300

    changed kBrushed to kBrushless

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,13 +8,14 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import static com.revrobotics.CANSparkBase.IdleMode.kBrake;
 import static com.revrobotics.CANSparkLowLevel.MotorType.kBrushed;
+import static com.revrobotics.CANSparkLowLevel.MotorType.kBrushless;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kForward;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.kReverse;
 import static edu.wpi.first.wpilibj.PneumaticsModuleType.CTREPCM;
 import static frc.robot.Constants.IntakeConstants.*;
 
 public class Intake extends SubsystemBase {
-    private final CANSparkMax intakeMotor = new CANSparkMax(INTAKE_MOTOR_ID, kBrushed);
+    private final CANSparkMax intakeMotor = new CANSparkMax(INTAKE_MOTOR_ID, kBrushless);
     private final DoubleSolenoid intakePiston = new DoubleSolenoid(CTREPCM, PISTON_FORWARD_CHANNEL, PISTON_REVERSE_CHANNEL);
 
     private final DigitalInput button = new DigitalInput(INTAKE_BUTTON_CHANNEL);
